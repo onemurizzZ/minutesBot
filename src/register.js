d25deb935631fd870ae038069960c00d5e54fa48
kaito <vvvvvvvvvvvvvv.lea@gmail.com>
2023-02-16 15:03:04 +0900

    remove unnecessary commands

--- src/register.js
+++ src/register.js
@@ -4,24 +4,24 @@ import dotenv from 'dotenv';
 // コマンド設定部分
 import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 
-const ping = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('pong!')
+// const ping = new SlashCommandBuilder()
+//     .setName('ping')
+//     .setDescription('pong!')
 
- //0.13.0以降
-const hello = new SlashCommandBuilder()
-    .setName('hello')
-    .setDescription('挨拶をします。')
-    .addStringOption(option =>
-        option
-            .setName('language')
-            .setDescription('言語を指定します。')
-            .setRequired(true) //trueで必須、falseで任意
-            .addChoices(
-                {name:'Japanese', value:'ja'},
-                {name:'English', value:'en'}
-            )
-    );
+//  //0.13.0以降
+// const hello = new SlashCommandBuilder()
+//     .setName('hello')
+//     .setDescription('挨拶をします。')
+//     .addStringOption(option =>
+//         option
+//             .setName('language')
+//             .setDescription('言語を指定します。')
+//             .setRequired(true) //trueで必須、falseで任意
+//             .addChoices(
+//                 {name:'Japanese', value:'ja'},
+//                 {name:'English', value:'en'}
+//             )
+//     );
 
 const minutes = new SlashCommandBuilder()
                 .setName('minutes')
@@ -33,7 +33,7 @@ const minutes = new SlashCommandBuilder()
                     .setRequired(false)
                 );
                 
-const commands = [ping, hello, minutes];
+const commands = [minutes];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 async function main(){
